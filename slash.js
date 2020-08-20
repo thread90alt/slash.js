3ce029f3368fb84053f497afe23abb1d72c28410
_thread90 <39356292+thread90@users.noreply.github.com>
2020-08-19 20:24:28 -0500

    Update slash.js

--- slash.js
+++ slash.js
@@ -1,5 +1,3 @@
-// NzIzMjQ1OTY0NjQyNzQ2NDM5.Xu5d4g.PB5NE2Y50hwQTvteN6KnXft6N0E
-
 const readline = require("readline");
 const Context = require("./context.js");
 const errors = require("./errors.js");
